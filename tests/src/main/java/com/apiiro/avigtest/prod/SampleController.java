9208afccaf6384f15f48c0888f0f45ab505617b9
avig-apiiro <avi.glatstein@apiiro.com>
2024-10-07 18:10:54 +0300

    wip

--- tests/src/main/java/com/apiiro/avigtest/prod/SampleController.java
+++ tests/src/main/java/com/apiiro/avigtest/prod/SampleController.java
@@ -13,7 +13,7 @@ public class SampleController {
     @RequestMapping(path = "startlesson.mvc", method = {RequestMethod.GET, RequestMethod.POST})
     public ModelAndView start() {
         var model = new ModelAndView();
-        int i=4;
+        int i=5;
 
         model.addObject("course", "c");
         model.addObject("lesson", "l");
