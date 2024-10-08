34b4b2987e0afcfe193550ef648e86a759e63932
avig-apiiro <avi.glatstein@apiiro.com>
2024-10-08 11:56:43 +0300

    sample controller 2

--- tests/src/main/java/com/apiiro/avigtest/prod/FooController.java
+++ tests/src/main/java/com/apiiro/avigtest/prod/FooController.java
@@ -1,5 +1,7 @@
 package com.apiiro.avigtest.prod;
 
+import org.springframework.context.annotation.Description;
+import org.springframework.context.annotation.EnableLoadTimeWeaving;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,10 @@ import java.util.Collection;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
 import javax.validation.constraints.Size;
+
+@EnableLoadTimeWeaving
+@Deprecated
+@Description("my lovely controller")
 @RestController
 @CrossOrigin(exposedHeaders = "errors, content-type")
 @RequestMapping(path = {
@@ -18,6 +24,8 @@ import javax.validation.constraints.Size;
 @Validated
 public class FooController {
 
+
+    @Validated(FooController.class)
     @PostMapping(value = "/address", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<?> addressVerification(@Valid @RequestBody @Size(max = 5) Collection<SampleDTO> model) {
         return new ResponseEntity<>(model.stream().map(m -> Integer.toString(m.val)).collect(Collectors.joining(",")), HttpStatus.OK);
