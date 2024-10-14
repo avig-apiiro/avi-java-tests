bf4e8eaad2320776ae1b5f04766eb7bd0d5c9444
avig-apiiro <avi.glatstein@apiiro.com>
2024-10-14 17:34:03 +0300

    move param to const

--- springdemo/src/main/java/com/apiiro/avigtest/springdemo/FirstController.java
+++ springdemo/src/main/java/com/apiiro/avigtest/springdemo/FirstController.java
@@ -11,27 +11,8 @@ import java.util.Map;
 @RestController("first")
 @RequestMapping("/api")
 public class FirstController {
-
-
-
     @GetMapping("/first")
     public String first() {
         return "hello world";
     }
-    @GetMapping("/path/{param}")
-    public Map<String, String> pathparam(@PathVariable String param) {
-        Map<String, String> response = new HashMap<>();
-        response.put("param", param);
-        return response;
-    }
-
-
-    @GetMapping("/example2/{" + Constants.PARAMNAME + "}")
-    public Map<String, String> getExample2(@PathVariable(Constants.PARAMNAME) String somevarname) {
-        Map<String, String> response = new HashMap<>();
-        response.put(Constants.PARAMNAME, somevarname);
-        return response;
-    }
-
-
 }
