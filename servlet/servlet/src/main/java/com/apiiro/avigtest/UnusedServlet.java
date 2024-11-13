db29559e8a65d385305f6ca096ddea57158544d0
avig-apiiro <avi.glatstein@apiiro.com>
2024-11-13 13:10:53 +0200

    java servlet project

--- servlet/servlet/src/main/java/com/apiiro/avigtest/UnusedServlet.java
+++ servlet/servlet/src/main/java/com/apiiro/avigtest/UnusedServlet.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class UnusedServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().println("UNUSED4");
+        response.getWriter().println("UNUSED5");
         response.setContentType("text/html");
         response.getWriter().close();
     }
