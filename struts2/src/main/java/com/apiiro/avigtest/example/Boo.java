c25b8c85c8d17b47d85afdda09c12ca0d4c5d3ee
avig-apiiro <avi.glatstein@apiiro.com>
2024-12-12 14:51:10 +0200

    s

--- struts2/src/main/java/com/apiiro/avigtest/example/Boo.java
+++ struts2/src/main/java/com/apiiro/avigtest/example/Boo.java
@@ -4,7 +4,7 @@ import com.opensymphony.xwork2.ActionSupport;
 
 public class Boo  extends ActionSupport {
 
-int i = 10;
+int i = 11;
     public String execute() throws Exception {
         return SUCCESS;
     }
