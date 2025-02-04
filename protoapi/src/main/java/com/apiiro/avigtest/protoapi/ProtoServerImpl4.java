5458034fe9a9f88d29ed4eba44a244f7f9b6a8fb
avig-apiiro <avi.glatstein@apiiro.com>
2025-02-04 10:41:49 +0200

    various creation algo

--- protoapi/src/main/java/com/apiiro/avigtest/protoapi/ProtoServerImpl4.java
+++ protoapi/src/main/java/com/apiiro/avigtest/protoapi/ProtoServerImpl4.java
@@ -5,6 +5,12 @@ import protoapi.HelloWorld;
 import protoapi.ProtoServer4Grpc;
 
 public class ProtoServerImpl4 extends ProtoServer4Grpc.ProtoServer4ImplBase {
+
+    @Override
+    public void getFullPerson(HelloWorld.PersonRequest4 request, StreamObserver<HelloWorld.Person4> responseObserver) {
+        responseObserver.onNext(buildPerson(request.getName(),"email", "1234"));
+    }
+
     @Override
     public void getPerson(HelloWorld.PersonRequest4 request, StreamObserver<HelloWorld.Person4> responseObserver) {
         String email = request.getName() + "@email.com";
