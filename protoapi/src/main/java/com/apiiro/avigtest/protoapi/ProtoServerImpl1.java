10f849a935d082778e3673f0afd03dc7428392d4
avig-apiiro <avi.glatstein@apiiro.com>
2025-02-04 13:20:22 +0200

    rename proto package

--- protoapi/src/main/java/com/apiiro/avigtest/protoapi/ProtoServerImpl1.java
+++ protoapi/src/main/java/com/apiiro/avigtest/protoapi/ProtoServerImpl1.java
@@ -1,26 +1,26 @@
 package com.apiiro.avigtest.protoapi;
 
 import io.grpc.stub.StreamObserver;
-import com.apiiro.proto.ProtoServer1Grpc;
-import com.apiiro.proto.Server1;
+import com.apiiro.proto.ProtoServerOneGrpc;
+import com.apiiro.proto.ServerOne;
 
-public class ProtoServerImpl1 extends ProtoServer1Grpc.ProtoServer1ImplBase {
+public class ProtoServerImpl1 extends ProtoServerOneGrpc.ProtoServerOneImplBase {
     @Override
-    public void getPerson(Server1.PersonRequest1 request, StreamObserver<Server1.Person1> responseObserver) {
+    public void getPerson(ServerOne.PersonRequest1 request, StreamObserver<ServerOne.PersonResponse1> responseObserver) {
         super.getPerson(request, responseObserver);
     }
 
     @Override
-    public void getFullPerson(Server1.PersonRequest1 request, StreamObserver<Server1.Person1> responseObserver) {
+    public void getFullPerson(ServerOne.PersonRequest1 request, StreamObserver<ServerOne.PersonResponse1> responseObserver) {
         String email = request.getName() + "@email.com";
-        Server1.Person1 reply = buildPerson(request.getName(), email, "1234");
+        ServerOne.PersonResponse1 reply = buildPerson(request.getName(), email, "1234");
         responseObserver.onNext(reply);
         responseObserver.onCompleted();
     }
 
 
-    public Server1.Person1 buildPerson(String name, String email, String creditCard) {
-        return Server1.Person1.newBuilder()
+    public ServerOne.PersonResponse1 buildPerson(String name, String email, String creditCard) {
+        return ServerOne.PersonResponse1.newBuilder()
             .setName(name)
             .setEmail(email)
             .setUsername(name)
