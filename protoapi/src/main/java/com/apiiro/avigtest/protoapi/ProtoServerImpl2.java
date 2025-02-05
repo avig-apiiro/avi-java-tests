0a434de61a64049b2239c876322b59b57f43102b
avig-apiiro <avi.glatstein@apiiro.com>
2025-02-05 16:43:20 +0200

    wip

--- protoapi/src/main/java/com/apiiro/avigtest/protoapi/ProtoServerImpl2.java
+++ protoapi/src/main/java/com/apiiro/avigtest/protoapi/ProtoServerImpl2.java
@@ -4,19 +4,19 @@ import io.grpc.stub.StreamObserver;
 
 public class ProtoServerImpl2 extends ProtoServer2Grpc.ProtoServer2ImplBase {
     @Override
-    public void getPerson(Server2.PersonRequest2 request, StreamObserver<Server2.Person2> responseObserver) {
+    public void getPerson(Server2.PersonRequest2 request, StreamObserver<Server2.Person> responseObserver) {
         super.getPerson(request, responseObserver);
     }
 
     @Override
-    public void getFullPerson(Server2.PersonRequest2 request, StreamObserver<Server2.Person2> responseObserver) {
+    public void getFullPerson(Server2.PersonRequest2 request, StreamObserver<Server2.Person> responseObserver) {
         String email = request.getName() + "@email.com";
-        Server2.Person2 reply = buildPerson(request.getName(), email, "1234");
+        Server2.Person reply = buildPerson(request.getName(), email, "1234");
         responseObserver.onNext(reply);
         responseObserver.onCompleted();    }
 
-    public Server2.Person2 buildPerson(String name, String email, String creditCard) {
-        return Server2.Person2.newBuilder()
+    public Server2.Person buildPerson(String name, String email, String creditCard) {
+        return Server2.Person.newBuilder()
             .setName(name)
             .setEmail(email)
             .setUsername(name)
