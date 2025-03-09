b92991b6483ca08e0a1e270896dc84ab38e7e6b4
avig-apiiro <avi.glatstein@apiiro.com>
2025-03-09 14:38:35 +0200

    s3 url

--- springdemo/src/main/java/com/apiiro/avigtest/springdemo/BeatelsController.java
+++ springdemo/src/main/java/com/apiiro/avigtest/springdemo/BeatelsController.java
@@ -25,6 +25,7 @@ public class BeatelsController {
     @GetMapping("/yellow" + "/submarine")
     public String yellowSubmarine(@RequestParam("value") String value) throws IOException {
         var cu =  new CloudinaryUploader("cloudName", "api-key", "api-secret");
+        String aws_url = "https:\\s3.us-east1.amazonaws.com\test";
         cu.uploadImage(value);
         return "yellow submarine";
     }
