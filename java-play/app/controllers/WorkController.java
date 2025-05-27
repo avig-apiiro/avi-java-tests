1469f879b277bb4d11e079bf0ff0bc868714b809
avig-apiiro <avi.glatstein@apiiro.com>
2025-05-27 16:42:22 +0300

    add pii

--- java-play/app/controllers/WorkController.java
+++ java-play/app/controllers/WorkController.java
@@ -1,9 +1,13 @@
 package controllers;
 
 import play.mvc.*;
+import services.UserManager;
 
 public class WorkController extends Controller {
     public Result index(String partial) {
+
+        UserManager userManager = new UserManager();
+        userManager.getUser(partial);
         return ok(views.html.index.render());
     }
 }
